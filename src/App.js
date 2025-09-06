0a974787221ac54d70043cd510d059f8edee41e8
Rhythm Winicour-Freeman <rwinicourfreeman@gmail.com>
2025-09-05 17:38:30 -0700

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import React, { useState } from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import DashboardPage from "./pages/DashboardPage";
 import TodosPage from "./pages/TodosPage";
@@ -18,20 +18,18 @@ function App() {
 
   return (
     // dynamically set className based on current state of darkMode
-    <Router>
-      <div className={`app ${darkMode ? 'dark' : 'light'}`}>
-        <Sidebar toggleTheme={toggleTheme} />
-        <main className="main-content">
-          <Routes>
-            {/* Landing Page = Dashboard */}
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/tasks" element={<TodosPage />} />
-            <Route path="/feedback" element={<FeedbackPage />} />
-            <Route path="*" element={<Navigate to ="/" replace />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className={`app ${darkMode ? 'dark' : 'light'}`}>
+      <Sidebar toggleTheme={toggleTheme} />
+      <main className="main-content">
+        <Routes>
+          {/* Landing Page = Dashboard */}
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/tasks" element={<TodosPage />} />
+          <Route path="/feedback" element={<FeedbackPage />} />
+          <Route path="*" element={<Navigate to ="/" replace />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
